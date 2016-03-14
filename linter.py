4380bb4249d84357d86d4123f33cd0ec4b265e47
AJ Michels <amichels@caringbridge.org>
2016-03-14 15:02:20 -0500

    add support for defining cmd on a per-project level
    * implements a custom `cmd()` method to check for the existence of an `cmd` setting
    * updated README documentation to show example of new functionality
    
    closes #10

--- linter.py
+++ linter.py
@@ -14,11 +14,11 @@ from SublimeLinter.lint import highlight, Linter
 
 
 class Phpmd(Linter):
-
     """Provides an interface to phpmd."""
 
     syntax = ('php', 'html', 'html 5')
     cmd = 'phpmd @ text'
+    executable = 'phpmd'
     regex = (
         r'(?P<filename>.+):(?P<line>\d+)'
         r'\s*(?P<message>.+)$'
@@ -30,3 +30,18 @@ class Phpmd(Linter):
     }
     inline_overrides = 'rulesets'
     comment_re = r'\s*<!--'
+
+    def cmd(self):
+        """Create the cmd."""
+        settings = Linter.get_view_settings(self)
+
+        # attempt to get the command from settings if it is present
+        if 'cmd' in settings:
+            command = [settings.get('cmd')]
+        else:
+            command = [self.executable_path]
+
+        command.append('@')
+        command.append('text')
+
+        return command
