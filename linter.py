0ac0bc12b2de425ad7de9a472466848216bacef7
Joyce Babu <joyce@ennexa.com>
2019-02-15 10:57:11 +0530

    Resolve path relative to working directory

--- linter.py
+++ linter.py
@@ -4,6 +4,7 @@ from SublimeLinter.lint import Linter, WARNING
 
 logger = logging.getLogger('SublimeLinter.plugins.phpmd')
 
+
 class Phpmd(Linter):
     cmd = ('phpmd', '${temp_file}', 'text')
     regex = (
@@ -17,50 +18,57 @@ class Phpmd(Linter):
     tempfile_suffix = 'php'
     defaults = {
         'selector': 'source.php, text.html.basic',
+        'rulesets:,': 'cleancode,codesize,controversial,design,naming,unusedcode'
     }
 
-    default_ruleset = 'cleancode,codesize,controversial,design,naming,unusedcode'
-
     def cmd(self):
-        cmd = ('phpmd', '${temp_file}', 'text')
+        settings = self.get_view_settings()
+        rulesets = settings.get('rulesets') or self.defaults['rulesets']
 
-        settings = Linter.get_view_settings(self)
-        rulesets = settings.get('rulesets')
-        
         if rulesets.endswith(".xml"):
-            rulesets_file = self.get_conf_file(rulesets)
-            if rulesets_file:
+            rulesets_file = self.resolve_rulesets_filename(rulesets)
+
+            if rulesets_file is not None:
                 return ('phpmd', '${temp_file}', 'text', rulesets_file)
 
             logger.error('file not found - %s' % rulesets)
-            rulesets = self.default_ruleset
-            
+
         return ('phpmd', '${temp_file}', 'text', rulesets)
 
-    def get_conf_file(self, filename):
+    def resolve_rulesets_filename(self, rulesets_file):
+        if os.path.basename(rulesets_file) is not rulesets_file:
+            if not os.path.isabs(rulesets_file):
+                settings = self.get_view_settings()
+                working_dir = self.get_working_dir(settings)
+                if working_dir is None:
+                    return None
 
-        if os.path.basename(filename) is not filename:
+                # Resolve path relative to current working dir
+                rulesets_file = os.path.abspath(os.path.join(working_dir, rulesets_file))
+
             # Absolute file path
-            return os.path.isfile(filename) and filename
+            if os.path.isfile(rulesets_file):
+                return rulesets_file
+
+            return None
 
         if not self.filename:
             # The file is not yet saved
-            return False
+            return None
 
         path = os.path.dirname(self.filename)
 
-        # Search for the file in directory tree
+        # Traverse up the directory tree and try to find the file
         while True:
-            newpath = os.path.dirname(path)
-
-            if path == newpath:
-                return False
-
-            file = os.path.join(path, filename)
-
+            file = os.path.join(path, rulesets_file)
             if os.path.isfile(file):
                 return file
 
+            newpath = os.path.dirname(path)
+            if path == newpath:
+                # We have reached the root directory
+                return None
+
             path = newpath
 
-        return False
+        return None
