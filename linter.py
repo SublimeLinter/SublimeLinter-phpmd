d31dd3322a8f843f10a049ca2954e63323d4f981
Koen Lageveen <koen@peppered.com>
2023-08-02 22:04:59 +0200

    turn into a real-files-only linter
    to support baseline files
    https://phpmd.org/documentation/index.html - section Baseline
    
    a baseline suppresses violations based on files,
    which does not work with our tempfile solution
    
    we don't need to capture filenames because this is a single-file linter

--- linter.py
+++ linter.py
@@ -2,16 +2,13 @@ from SublimeLinter.lint import Linter, WARNING
 
 
 class Phpmd(Linter):
-    cmd = ('phpmd', '${temp_file}', 'text')
+    cmd = ('phpmd', '${file}', 'text')
     regex = (
-        # For now, do *NOT* capture 'filename' since phpmd reports 'real'
-        # paths, and Sublime and SL cannot map such paths to the original
-        # `view.file_name()`.
         r'(.+):(?P<line>\d+)\s*(?P<message>.+)$'
     )
     on_stderr = None  # handle stderr via regex
     default_type = WARNING
-    tempfile_suffix = 'php'
+    tempfile_suffix = '-'
     defaults = {
         'selector': 'embedding.php, source.php',
         '@rulesets:,': 'cleancode,codesize,controversial,design,naming,unusedcode'
